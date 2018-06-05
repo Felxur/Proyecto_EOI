3cd4a5affa3763c70fa6220abe65e9f0fe8d382d
Miguel Verdu muñoz <miguel.verdu90@gmail.com>
2018-06-06 01:28:19 +0200

    Añadido jugador con: movimiento y seguimiento del curso y script de camara, falta animaciones de movimiento

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    private Animator animator;
+    public float moveSpeed;
+    bool isReloading=false;
+    bool isRunning=false;
+
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+
+    }
+    void SetIsReloadingFalse()
+    {
+        isReloading = false;
+        animator.SetBool("IsReloading", false);
+    }
+    private void FixedUpdate()
+    {
+     
+    }
+    // Update is called once per frame
+    void Update () {
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * moveSpeed * Time.deltaTime, Space.World);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.up * moveSpeed * Time.deltaTime, Space.World);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * moveSpeed * Time.deltaTime, Space.World);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.down * moveSpeed * Time.deltaTime, Space.World);
+        }
+
+    }
+}
---
+++ Assets/Scripts/RotateToCursor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateToCursor : MonoBehaviour {
+    Vector3 mousePos;
+    Camera cam;
+    Rigidbody2D rigid;
+
+	// Use this for initialization
+	void Start () {
+        rigid = this.GetComponent<Rigidbody2D>();
+        cam = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rotateToCamera();
+		
+	}
+    void rotateToCamera()
+    {
+        mousePos = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z - cam.transform.position.z));
+        rigid.transform.eulerAngles = new Vector3(0, 0, Mathf.Atan2((mousePos.y - transform.position.y), (mousePos.x - transform.position.x)) * Mathf.Rad2Deg);
+    }
+
+}
