b86f592436340b8d1cbf493496db213f4720a49e
Miguel Verdu muñoz <miguel.verdu90@gmail.com>
2018-06-19 17:41:59 +0200

    creado generador de zombies, pendiente arregar el dodge de los zombies

---
+++ Assets/Scripts/Enemy&Instantiator/Instantiator.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instantiator : MonoBehaviour {
+    private bool activator;
+    public GameObject enemyPrefab;
+    private Rigidbody2D instantiatorRigid;
+    public int quantity=1;
+    [Tooltip("Segundos de Retraso entre Zombies")]
+    public float delay=0f;
+    [Tooltip("si marcas repeat se generaran mas zombies si se vuelve a activar 'trigger' una vez termina la primera ronda")]
+    public bool repeat = false;
+    private float currentTime;
+
+    
+    void Start () {
+        instantiatorRigid = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        activator = Trigger.activate;
+        if (activator==true)
+        {
+            currentTime += Time.deltaTime;
+            if (currentTime >= delay && quantity>0)
+            {
+                currentTime = 0f;
+                Instantiate(enemyPrefab);
+                quantity--;
+            }else if (quantity == 0 && repeat==true)
+            {
+                Trigger.activate = false;
+            }
+        }
+
+        
+		
+	}
+}
---
+++ Assets/Scripts/Enemy&Instantiator/Trigger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger : MonoBehaviour {
+    public GameObject instantiator;
+    public static bool activate = false;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       
+		
+	}
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        activate = true;
+
+    }
+}
